bcc2ca4d9292aa8ddccd135f769f39d612ad0a56
mukaportes <mport.dev@gmail.com>
2019-08-19 00:01:41 -0300

    Renamed component name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavBar from './components/navBar';
+import NavBar from './components/commons/nav-bar';
 
 // this.props.children is where the child components we declare in our router will be rendered.
 class App extends Component {
