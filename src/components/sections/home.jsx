47a78259138a4da08a3deb4542225fe0d2303fec
mukaportes <mport.dev@gmail.com>
2019-08-19 23:42:30 -0300

    Add phrases list for testing text fx

--- src/components/sections/home.jsx
+++ src/components/sections/home.jsx
@@ -6,27 +6,43 @@ import {
   Button
 } from 'semantic-ui-react';
 
+const phrases = [
+  'Needs consulting services to enhance code quality?',
+  'Needs a full-stack software engineer to build your idea from scratch?',
+  'Needs a software engineer to refactor your codebase?',
+  'Needs a software engineer to set a pattern to your codebase?',
+  'Needs a programmer to review your codebase?',
+  'Needs a software engineer to help with an ongoing project?',
+];
+
 class HomeSection extends Component {
+  constructor() {
+    super();
+    this.state = { currentPhrase: 0, phrases };
+  }
+
+  componentDidMount() {
+    var thiz = this;
+    let phrases = this.state.phrases;
+    let currentPhrase = this.state.currentPhrase;
+    
+    setInterval(function () {
+      let newPhrase = currentPhrase += 1;
+      if (newPhrase > phrases.length) newPhrase = 0;
+
+      thiz.setState({ phrases, currentPhrase: newPhrase });
+    }, 1000);
+  }
+
   render() {
+    let content = this.state.currentPhrase;
+
     return (
       <Container text>
-        <Header
-          as='h1'
-          content='MUKAPORTES'
-          className='home-header'
-          inverted
-        />
-        <Header
-          as='h2'
-          content='Software consulting services'
-          className='home-sub-header'
-          inverted
-        />
-        <Button primary size='huge'>
-          Learn more
-      <Icon name='right arrow' />
-        </Button>
-      </Container>
+        <div class="typewriter">
+          <h1>{content}</h1>
+        </div>
+      </Container >
     );
   }
 }
